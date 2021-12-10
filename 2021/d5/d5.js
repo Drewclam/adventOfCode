d3a6f2d55ebf9a2a58a68f358d7e573381ca458c
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-12-09 20:09:40 -0500

    Try part 2

--- 2021/d5/d5.js
+++ 2021/d5/d5.js
@@ -1,65 +1,46 @@
 const parse = input => input.split('\n').map(str => str.split('').map(Number));
 
+// [up, down, left, right]
 const getAdjacent = (row, col, arr) => {
+    const down = arr[row + 1]?.[col];
+    const right = arr[row]?.[col + 1];
+    const left = arr[row]?.[col - 1];
+    const up = arr[row - 1]?.[col];
     if (row === 0) {
         if (col === 0) {
-            const down = arr[row + 1][col];
-            const right = arr[row][col + 1];
-            return [down, right];
+            return [null, down, null, right];
         } else if (col === arr[row].length - 1) {
-            const down = arr[row + 1][col];
-            const left = arr[row][col - 1];
-            return [down, left];
+            return [null, down, left, null];
         } else {
-            const left = arr[row][col - 1];
-            const down = arr[row + 1][col];
-            const right = arr[row][col + 1];
-            return [down, right, left];
+            return [null, down, left, right];
         }
     } else if (row === arr.length - 1) {
         if (col === 0) {
-            const up = arr[row - 1][col];
-            const right = arr[row][col + 1];
-            return [up, right];
+            return [up, null, null, right];
         } else if (col === arr[row].length - 1) {
-            const up = arr[row - 1][col];
-            const left = arr[row][col - 1];
-            return [up, left];
+            return [up, null, left, null];
         } else {
-            const left = arr[row][col - 1];
-            const up = arr[row - 1][col];
-            const right = arr[row][col + 1];
-            return [up, right, left];
+            return [up, null, left, right];
         }
-    } else {
-        if (col === 0) {
-            const down = arr[row + 1][col];
-            const up = arr[row - 1][col];
-            const right = arr[row][col + 1];
-            return [up, down, right];
-        } else if (col === arr[row].length - 1) {
-            const down = arr[row + 1][col];
-            const up = arr[row - 1][col];
-            const left = arr[row][col - 1];
-            return [up, down, left];
-        }
-        const down = arr[row + 1][col];
-        const up = arr[row - 1][col];
-        const right = arr[row][col + 1];
-        const left = arr[row][col - 1];
-
-        return [up, down, left, right];
     }
+
+    if (col === 0) {
+        return [up, down, null, right];
+    } else if (col === arr[row].length - 1) {
+        return [up, down, left, null];
+    }
+
+    return [up, down, left, right];
 }
 
-const isLowest = (val, nums) => val < nums.sort((a, b) => a - b)[0];
+const isLowest = (val, nums) => val < nums.filter(x => x !== null).sort((a, b) => a - b)[0];
 
 const getRiskLevelsSum = (lowPoints) => lowPoints.reduce((sum, lowPoint) => sum += (lowPoint + 1), 0);
 
 const solveP1 = (arr) => {
     const lowest = [];
     for (let row = 0; row < arr.length; row++) {
-        for (let col = 0; col < arr[row].length; col ++) {
+        for (let col = 0; col < arr[row].length; col++) {
             const adjacentNums = getAdjacent(row, col, arr);
             const height = arr[row][col];
             if (isLowest(height, adjacentNums)) {
@@ -68,5 +49,69 @@ const solveP1 = (arr) => {
         }
     }
     return getRiskLevelsSum(lowest);
-} 
-module.exports = { parse, getAdjacent, isLowest, getRiskLevelsSum, solveP1 };
+}
+
+const Location = (row, col, val) => ({ row, col, val });
+
+const getBasin = (row, col, arr) => {
+    const res = [Location(row, col, arr[row][col])];
+    const addLocation = (row, col, val) => {
+        const location = Location(row, col, val);
+        const hasLocation = res.filter(r => r.row === location.row && r.col === location.col && r.val === location.val);
+        if (hasLocation.length < 1) {
+            res.push(location);
+        }
+    }
+    const recurse = (row, col, arr, currentNum) => {
+        const adjacentNums = getAdjacent(row, col, arr);
+        for (let i = 0; i < adjacentNums.length; i++) {
+            const num = adjacentNums[i];
+            // console.log("row:",row, "\ncol",col,"\ni:",i, "\ncurrentNum:", currentNum, "\nadjacent:", adjacentNums);
+            if (num === null || num === undefined) {
+                continue;
+            }
+            if (num === currentNum + 1 && num < 9) {
+                if (i === 0) {
+                    addLocation(row - 1, col, num)
+                    recurse(row - 1, col, arr, num);
+
+                } else if (i === 1) {
+                    addLocation(row + 1, col, num)
+                    recurse(row + 1, col, arr, num);
+
+                } else if (i === 2) {
+                    addLocation(row, col - 1, num)
+                    recurse(row, col - 1, arr, num);
+
+                } else if (i === 3) {
+                    addLocation(row, col + 1, num)
+                    recurse(row, col + 1, arr, num);
+                }
+            }
+        }
+    }
+    recurse(row, col, arr, arr[row][col]);
+    return res.map(({ val }) => val);
+}
+
+const solveP2 = (arr) => {
+    const LowPoint = (row, col, val) => ({ row, col, val });
+    const lowest = [];
+    for (let row = 0; row < arr.length; row++) {
+        for (let col = 0; col < arr[row].length; col++) {
+            const adjacentNums = getAdjacent(row, col, arr);
+            const height = arr[row][col];
+            if (isLowest(height, adjacentNums)) {
+                lowest.push(LowPoint(row, col, height));
+            }
+        }
+    }
+    return lowest
+        .map(({ row, col }) => getBasin(row, col, arr).length)
+        .sort((a, b) => a - b)
+        .reverse()
+        .slice(0, 3)
+        .reduce((product, num) => product *= num)
+
+}
+module.exports = { parse, getAdjacent, isLowest, getRiskLevelsSum, solveP1, getBasin, solveP2 };
--- 2021/d5/d5.spec.js
+++ 2021/d5/d5.spec.js
@@ -1,5 +1,5 @@
 const { sample, input } = require('./input');
-const { parse, getAdjacent, isLowest, getRiskLevelsSum, solveP1 } = require('./d5');
+const { parse, getAdjacent, isLowest, getRiskLevelsSum, solveP1, getBasin, solveP2 } = require('./d5');
 
 const parsedSample = parse(sample);
 const parsedInput = parse(input);
@@ -17,45 +17,27 @@ describe('d5', () => {
 
     it('gets the adjacent numbers', () => {
         // corners
-        expect(getAdjacent(0, 0, parsedSample)).toContain(1, 3);
-        expect(getAdjacent(0, 0, parsedSample)).toHaveLength(2);
-
-        expect(getAdjacent(0, 9, parsedSample)).toContain(1, 1);
-        expect(getAdjacent(0, 9, parsedSample)).toHaveLength(2);
-
-        expect(getAdjacent(4, 0, parsedSample)).toContain(8, 8);
-        expect(getAdjacent(4, 0, parsedSample)).toHaveLength(2);
-
-        expect(getAdjacent(4, 9, parsedSample)).toContain(9, 7);
-        expect(getAdjacent(4, 9, parsedSample)).toHaveLength(2);
-
+        expect(getAdjacent(0, 0, parsedSample)).toEqual([null, 3, null, 1]);
+        expect(getAdjacent(0, 9, parsedSample)).toEqual([null, 1, 1, null]);
+        expect(getAdjacent(4, 0, parsedSample)).toEqual([8, null, null, 8]);
+        expect(getAdjacent(4, 9, parsedSample)).toEqual([9, null, 7, null]);
         // edges
-        expect(getAdjacent(0, 4, parsedSample)).toContain(9, 8, 4);
-        expect(getAdjacent(0, 4, parsedSample)).toHaveLength(3);
-
-        expect(getAdjacent(2, 9, parsedSample)).toContain(1, 9, 9);
-        expect(getAdjacent(2, 9, parsedSample)).toHaveLength(3);
-
-        expect(getAdjacent(4, 4, parsedSample)).toContain(9, 8, 6);
-        expect(getAdjacent(4, 4, parsedSample)).toHaveLength(3);
-
-        expect(getAdjacent(2, 0, parsedSample)).toContain(3, 8, 8);
-        expect(getAdjacent(2, 0, parsedSample)).toHaveLength(3);
-
+        expect(getAdjacent(0, 4, parsedSample)).toEqual([null, 8, 9, 4]);
+        expect(getAdjacent(2, 9, parsedSample)).toEqual([1, 9, 9, null]);
+        expect(getAdjacent(4, 4, parsedSample)).toEqual([8, null, 9, 6]);
+        expect(getAdjacent(2, 0, parsedSample)).toEqual([3, 8, null, 8]);
         // normal
-        expect(getAdjacent(1, 1, parsedSample)).toContain(1, 3, 8, 8);
-        expect(getAdjacent(1, 1, parsedSample)).toHaveLength(4);
-
-        expect(getAdjacent(3, 4, parsedSample)).toContain(7, 7, 9, 9);
-        expect(getAdjacent(3, 4, parsedSample)).toHaveLength(4);
+        expect(getAdjacent(1, 1, parsedSample)).toEqual([1, 8, 3, 8]);
+        expect(getAdjacent(3, 4, parsedSample)).toEqual([7, 9, 7, 9]);
     });
 
     it('checks if lowest', () => {
-        expect(isLowest(1, [9, 9, 2])).toBe(true);
+        expect(isLowest(1, [9, 9, 2, null])).toBe(true);
         expect(isLowest(5, [8, 8, 6, 6])).toBe(true);
-        expect(isLowest(0, [1, 1])).toBe(true);
-        expect(isLowest(5, [6, 6, 6])).toBe(true);
-        expect(isLowest(2, [1, 3])).toBe(false);
+        expect(isLowest(0, [null, 1, 1, null])).toBe(true);
+        expect(isLowest(5, [6, null, 6, 6])).toBe(true);
+        expect(isLowest(2, [1, 3, null, undefined])).toBe(false);
+        expect(isLowest(2, [4, 3, null, undefined])).toBe(true);
     });
 
     it('gets sum of risk levels', () => {
@@ -65,5 +47,38 @@ describe('d5', () => {
     it('solves part 1', () => {
         expect(solveP1(parsedSample)).toBe(15);
         expect(solveP1(parsedInput)).toBe(588);
+    })
+
+    it('gets the basin for a height', () => {
+        expect(getBasin(0, 1, [[1, 2, 3, 4, 5]])).toEqual([2, 3, 4, 5]);
+        expect(getBasin(0, 4, [[5,4,3,2,1]])).toEqual([1,2,3,4,5]);
+        expect(getBasin(0, 0, [
+            [3, 4, 3, 2, 1],
+            [4, 1, 2, 3, 4]
+        ])).toEqual([3, 4, 4]);
+        expect(getBasin(1, 2, [
+            [3, 5, 4, 2, 1],
+            [4, 1, 3, 3, 4]
+        ])).toEqual([3, 4, 5]);
+        expect(getBasin(1, 2, [
+            [3, 5, 4, 2, 1],
+            [4, 1, 1, 2, 4],
+            [4, 3, 4, 3, 4]
+        ])).toEqual([1, 2, 3, 4, 4]);
+        expect(getBasin(1, 2, [
+            [3, 5, 4, 2, 1],
+            [4, 1, 1, 2, 4],
+            [4, 3, 4, 3, 4]
+        ])).toEqual([1, 2, 3, 4, 4]);
+
+        expect(getBasin(0, 1, parsedSample)).toEqual([1, 2, 3]);
+        expect(getBasin(0, 9, parsedSample)).toEqual([0, 1, 2, 2, 1, 2, 3, 4 , 4]);
+        expect(getBasin(2, 2, parsedSample)).toEqual([5, 6, 7, 8, 8, 8, 7, 8, 6, 7, 8 , 8,7, 8]);
+        expect(getBasin(4, 6, parsedSample)).toEqual([5, 6, 7, 8, 8, 6, 6,7 ,8]);
+    })
+
+    it('solves part 2', () => {
+        expect(solveP2(parsedSample)).toBe(1134);
+        expect(solveP2(parsedInput)).toBe(1134);
     })
 })
