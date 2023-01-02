f3e4d28c73f94c9fbdf4a905d251f0571fab9ef6
andrew.lam@rangle.io <andrew.lam@rangle.io>
2023-01-01 20:00:39 -0500

    Add day 9 part 1

---
+++ 2022/d9/d9.ts
@@ -0,0 +1,65 @@
+import { parseFile } from 'utils';
+
+type Position = {
+  x: number;
+  y: number;
+};
+
+const parseInstruction = (input: string): [string, number] => {
+  const [direction, val] = input.split(' ');
+  return [direction, Number(val)];
+};
+
+const shouldUpdateTail = (head: Position, tail: Position) =>
+  Math.abs(head.x - tail.x) > 1 || Math.abs(head.y - tail.y) > 1;
+
+const update = (head: Position, tail: Position, newHeadPos: Position) => {
+  if (shouldUpdateTail(newHeadPos, tail)) {
+    tail.x = head.x;
+    tail.y = head.y;
+  }
+  head.x = newHeadPos.x;
+  head.y = newHeadPos.y;
+};
+
+const move = (direction: string, value: number, head: Position, tail: Position, visited: any) => {
+  for (let i = 0; i < value; i++) {
+    switch (direction) {
+      case 'R':
+        update(head, tail, { ...head, x: head.x + 1 });
+        break;
+      case 'D':
+        update(head, tail, { ...head, y: head.y - 1 });
+        break;
+      case 'L':
+        update(head, tail, { ...head, x: head.x - 1 });
+        break;
+      case 'U':
+        update(head, tail, { ...head, y: head.y + 1 });
+        break;
+      default:
+        break;
+    }
+    updateVisited(visited, tail);
+  }
+  // console.log({ head, tail });
+};
+
+const updateVisited = (visited: Position[], tail: Position) => {
+  const isVisited = visited.some((pos) => tail.x === pos.x && pos.y === tail.y);
+  if (!isVisited) {
+    visited.push({ ...tail });
+  }
+};
+
+parseFile('input.txt').then((content) => {
+  const head: Position = { x: 0, y: 0 };
+  const tail: Position = { x: 0, y: 0 };
+  const visited = [{ ...tail }];
+
+  content.split('\n').forEach((line) => {
+    const [direction, value] = parseInstruction(line);
+    move(direction, value, head, tail, visited);
+  });
+  console.log(visited.length);
+});
